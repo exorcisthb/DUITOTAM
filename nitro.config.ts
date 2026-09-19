c168e23699ea9b05ce5815791df71c6f55f063c3
exorcisthb <exorcisthb@gmail.com>
2026-09-19 14:27:05 +0700

    chore: update nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,7 +3,7 @@ import { defineNitroConfig } from "nitro/config";
 export default defineNitroConfig({
   rollupConfig: {
     output: {
-      // @ts-expect-error rolldown specific option
+      // rolldown specific option
       codeSplitting: false,
       inlineDynamicImports: true,
     },
